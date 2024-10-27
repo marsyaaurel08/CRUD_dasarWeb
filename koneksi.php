b7b74b12a6a512eed8c924f3ee01c4bc515b08cb
marsyaaurel08 <sefiramarsya@gmail.com>
2024-10-27 20:51:08 +0700

    .

--- koneksi.php
+++ koneksi.php
@@ -1,12 +1,12 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$dbname = "katalog_jilbab";
+//$host = "localhost";
+//$user = "root";
+//$pass = "";
+//$dbname = "katalog_jilbab";
 
-$conn = new mysqli($host, $user, $pass, $dbname);
+//$conn = new mysqli($host, $user, $pass, $dbname);
 
-if ($conn->connect_error) {
-    die("Koneksi gagal: " . $conn->connect_error);
-}
+//if ($conn->connect_error) {
+  //  die("Koneksi gagal: " . $conn->connect_error);
+//}
 ?>
